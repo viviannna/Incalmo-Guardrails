07e7f4c95fd1919b1c1cc77b2949ca211c7ad968
peacock4o <mpmorris@andrew.cmu.edu>
2025-10-30 17:44:10 -0400

    Fixed celery permission errors and sqlite file persistence

--- docker/attacker/start.sh
+++ docker/attacker/start.sh
@@ -6,9 +6,18 @@ if [ "$MODE" == "docker" ]; then
   ./sandcat.go -server http://$SERVER_IP:8888 -group red &
 fi
 
+CELERY_STATE_DIR="/tmp/celery_state"
+mkdir -p "$CELERY_STATE_DIR"
+chmod 777 "$CELERY_STATE_DIR"
 
 cd /incalmo
-uv run celery -A incalmo.c2server.celery.celery_worker worker --concurrency=1 &
-uv run celery -A incalmo.c2server.celery.celery_worker beat &
+
+uv run celery -A incalmo.c2server.celery.celery_worker worker \
+  --concurrency=1 \
+  --statedb "$CELERY_STATE_DIR/celery.db" &
+
+uv run celery -A incalmo.c2server.celery.celery_worker beat \
+  --schedule "$CELERY_STATE_DIR/celerybeat-schedule" &
+  
 sleep 3
 uv run ./incalmo/c2server/c2server.py
